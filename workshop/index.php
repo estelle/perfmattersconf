53fae3d366b2cd7f1e39cb3c21e871f09948b0ee
Estelle <eweyl@instartlogic.com>
2017-12-20 12:19:10 -0800

    more edits

---
+++ workshop/index.php
@@ -0,0 +1,104 @@
+<?php include '../test.inc' ?><!doctype html>
+<html lang="en">
+<head>
+	<title>#PerfMatters: Getting Up To Speed</title>
+	<link rel="stylesheet" href="../css/style2.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+	
+</head>
+<body id="home">
+<nav>
+    <?php echo $logo ?>
+	<?php echo $nav ?>
+</nav>
+<header>
+  <hgroup>
+<div class="circle"  style="padding-top:2.5em">
+	<h1>Workshop</h1>
+	<h2><br/>#PerfMatters Conference<br/>
+	Redwood City, CA <br/>
+	<?php echo $wk_date ?></h2>
+</div>
+<div>
+<h1>Getting Up To Speed with Performance</h1><p>Monday, March 26, 2018, we're offering a full day workshop to "Get You Up To Speed" on Web Performance. Participants will learn everything they need to know to get the most out of #PerfMatters Conference, the 2-day conference dedicated to front-end web performance</p>
+<p>Here's what we're planning:</p>
+</div></hgroup></header>
+<main>
+
+<section>
+<h1>Getting Up to Speed with Performance</h1>
+
+<p>Websites may be slow, but the web itself moves fast. Every day new techniques and tools emerge, promising to make your site faster and lighter. New articles are written about the latest "best practices" in performance, only to have yet newer articles contradict them. When the technologies and techniques we use to make websites more performant change so quickly, it's easy to feel like you're falling behind.</p>
+
+<p>But if you pull back the layers and the hype, drilling into the core concepts of how the network and browsers we rely on actually work, it suddenly becomes much easier to recognize which best practices are actually appropriate for you, and what trade-offs you're making.</p>
+
+<p>In this workshop, we'll go beyond the latest and greatest techniques and tools to understand what is going on at the core. We'll explore: </p>
+
+<ul>
+  <li>How the browser renders pages, from the very first request on the network to the final pixel on your screen</li>
+  <li>What the critical rendering path is and how to optimize for it</li>
+  <li>How HTTP/2 differs from HTTP/1 and what that means for popular opimizations</li>
+  <li>How to identify common performance bottlenecks in your sites and applications</li></ul>
+
+<p>By the time we're done, you'll have a firm understanding of what makes sites fast, and how to separate the hype from reality.</p>
+</section>
+<section class="reg" id="reg">
+  <h1>Register for "Getting Up To Speed" Workshop</h1>
+  <p>March 6, 2018 at Cañada College</p>
+  <table class="pop">
+    <thead><tr>
+      <th>Ticket Type</th>
+      <th>Cost</th>
+    </tr></thead>
+    <thead><tr><th colspan="2">Regular Tickets (thru January 31)</th></tr></thead>
+    <tbody>
+    <tr>
+      <td>Conference &amp; Workshop</td>
+      <td>$549</td>
+    </tr>
+
+    <tr>
+      <td>Workshop Only</td>
+      <td>$200</td>
+    </tr>
+    <tr>
+      <td>Conference Only</td>
+      <td>$399</td>
+    </tr>
+  </tbody>
+    <thead><tr><th colspan="2">Late Bird Tickets (thru March 24) &nbsp; &nbsp; &nbsp;</th></tr></thead>
+    <tbody>
+    <tr>
+      <td>Conference &amp; Workshop</td>
+      <td>$679</td>
+    </tr>
+    <tr>
+      <td>Workshop Only</td>
+      <td>$250</td>
+    </tr>
+    <tr>
+      <td>Conference Only</td>
+      <td>$479</td>
+    </tr>
+   
+  </tbody>
+  <tfoot>
+    <tr>
+      <td colspan="2"><a href="https://perfmattersconf.lilregie.com" title="Register Now" class="cta">Register Now</a></td>
+    </tr>
+  </tfoot>
+</table>
+
+<p><small>Prices subject to change. No refunds. Tickets are transferable with notice. Questions? Contact us at <a href="mailto:tickets@perfmattersconf.com">tickets@perfmattersconf.com</a></small>
+</section>
+<section><div>
+<h2 style="text-align: left;"><img src="../avatars/tim.jpg" alt="Tim Kadlec" class="tim">About Tim Kadlec</h2>
+<p>Tim is&nbsp;head of developer relations at&nbsp;<a href="https://snyk.io/" target="foo">Snyk</a>—a company focused on making open source code more secure. He is the author of&nbsp;<a href="http://implementingresponsivedesign.com/" target="foo">Implementing Responsive Design: Building sites for an anywhere, everywhere web</a>,&nbsp;and was a contributing author for&nbsp;<a href="https://shop.smashingmagazine.com/smashing-book-4-ebooks.html" target="foo">Smashing Book #4: New Perspectives on Web Design</a>,&nbsp;and the&nbsp;<a href="http://www.amazon.com/Web-Performance-Daybook-Volume-2/dp/1449332919" target="foo">Web Performance Daybook Volume 2</a>. Along with Katie Kovalcin, he&nbsp;also co-hosts&nbsp;<a href="http://pathtoperf.com/" target="foo">The Path to Performance Podcast</a>.</p></div>
+</section>
+</main>
+<footer>
+	<?php echo $nav ?>
+	<?php echo $footer ?>
+</footer>
+</body>
+</html>
